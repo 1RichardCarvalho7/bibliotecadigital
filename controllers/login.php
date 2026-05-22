e70fb750160b5995affbba1271436aadff894f6f
rafs00078000-eng <rafs00078000@gmail.com>
2026-05-21 22:16:54 -0300

    v5.2

--- controllers/login.php
+++ controllers/login.php
@@ -5,18 +5,22 @@ session_start();
 $nome = $_POST["nome"];
 $senha = $_POST["senha"];
 
-$arquivo =  __DIR__ . "../data/users.json";
-if ((file_get_contents(($arquivo) != null)) && !file_exists($arquivo)) {
+$arquivo =  __DIR__ . "/../data/users.json";
+if (file_exists($arquivo)) {
     $conteudo = file_get_contents($arquivo); // Coleta o conteúdo do arquivo (em json)
 } else{die("Nenhum usuário cadastrado.");}
 $usuarios_json = json_decode($conteudo, true); // Converte o conteúdo em json para o padrão que o php entende (array associativo) O true faz o objeto ser convertido em array associativo. Quando está definido como falso retorna um objeto (padrão: false)
+
+if (!$usuarios_json){
+    die("Arquivo de usuários inválido.")
+}
 
 if (!file_exists($arquivo)) {
     die("Nenhum usuário cadastrado.");
 }
 
 foreach ($usuarios_json as $usuario){
-    if($usuarios_json['nome'] === $nome && password_verify($senha, $usuarios_json['senha'])){ ## Verifica se o nome e a senha correspondem ao nome e a senha (em hash) do arquivo users.json
+    if($usuario['nome'] === $nome && password_verify($senha, $usuario['senha'])){ ## Verifica se o nome e a senha correspondem ao nome e a senha (em hash) do arquivo users.json
 
        $_SESSION['usuario'] = $usuario['nome']; ## Salva a sessão com o nome de usuário correspondente
 
