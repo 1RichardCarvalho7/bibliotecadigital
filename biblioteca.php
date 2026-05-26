b899921ed9e259ad607e9aecb1cecc26857258b5
rafs00078000-eng <rafs00078000@gmail.com>
2026-05-26 10:23:49 -0300

    v5.3.3

--- biblioteca.php
+++ biblioteca.php
@@ -1,3 +1,19 @@
-<?php 
-include_once 'includes/header.php';
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Biblioteca</title>
+    <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include_once 'includes/header.php';?>
+
+    <div id="upload-popup" class="overlay">
+        
+    </div>
+
+    <?php include_once 'includes/footer.php';?>
+</body>
+</html>
--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minha Biblioteca</title>
-    <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
-</head>
-<body>
+
     <header>
         <nav>
-            <a href="#"><img src="../assets/imagens/uploadIcon.png" alt="Upload"></a>
+            <a href="#upload-popup"><img src="../assets/imagens/uploadIcon.png" alt="Upload"></a>
         </nav>
     </header>
