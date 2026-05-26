e8b39507675e08e8ce1ad764073e042ceacf6e7b
gabss1509 <gabrielsupesseu@gmail.com>
2026-05-26 10:56:12 -0300

    Merge branch 'main' of https://github.com/1RichardCarvalho7/bibliotecadigital

--- biblioteca.php
+++ biblioteca.php
@@ -11,7 +11,13 @@
     <?php include_once 'includes/header.php';?>
 
     <div id="upload-popup" class="overlay">
-        
+        <form action="controllers/upload.php" method="POST" enctype="multipart/form-data">
+            <label for="imagem">coloque a capa do livro:</label>
+            <input type="file" name="imagem">
+            <label for="nome-autor">coloque o nome do autor:</label>
+            <input type="text" name="nome-autor">
+            <button type="submit">enviar</button>
+        </form>
     </div>
 
     <?php include_once 'includes/footer.php';?>
