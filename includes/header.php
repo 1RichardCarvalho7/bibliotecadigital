8cacb54780b513a89706d63affef0a70bca3b73e
rafs00078000-eng <rafs00078000@gmail.com>
2026-05-26 10:13:49 -0300

    v5.3.2

--- includes/header.php
+++ includes/header.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Biblioteca Digital</title>
+    <title>Minha Biblioteca</title>
+    <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
 </head>
 <body>
     <header>
         <nav>
-            <a href="#">Início</a>
+            <a href="#"><img src="../assets/imagens/uploadIcon.png" alt="Upload"></a>
         </nav>
     </header>
