178d32078e4d0c8a29971801d92a59e97bebda65
1RichardCarvalho7 <carvalho710904@gmail.com>
2026-05-27 17:35:53 -0300

    ok

--- login.php
+++ login.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="assets/css/loginEcadastro.css">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@100..900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h2>Entre em sua conta</h2>
+    <h2 class="titulo-login">Entre em sua conta!</h2>
     <form action="controllers/login.php" method="post">
         <label for="nome">Nome</label>
         <input type="text" name="nome" required>
