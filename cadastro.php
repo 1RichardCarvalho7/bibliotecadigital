d4508fd9e933b4fe7e8d5138e4dcff7e3007a0c6
1RichardCarvalho7 <Carvalho710904@gmail.com>
2026-06-04 21:43:08 -0300

    grrr

--- cadastro.php
+++ cadastro.php
@@ -1,18 +1,44 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <link rel="stylesheet" href="assets/css/loginEcadastro.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
+
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="assets/css/loginEcadastro.css">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@100..900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h2>Cadastro</h2>
-    <form action="controllers/cadastro.php" method="post">
-        <label for="nome">Nome</label>
-        <input type="text" name="nome" required>
-        <label for="senha">Senha</label>
-        <input type="text" name="senha" required>
-        <button type="submit">Cadastrar</button> 
-    </form>
+    <div class="container">
+        <div class="left">
+            <img src="assets/imagens/favicon.png" alt="Logo">
+            <div class="titulo">
+                <span>Bibliolandy</span>
+            </div>
+            <h1 class="textao">Crie sua conta!</h1>
+        </div>
+    <div class="right">
+        <div class="chiclete">
+            <div class="card">
+            <form action="controllers/cadastro.php" method="post">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" required>
+            <label for="senha">Senha</label>
+            <input type="text" name="senha" required>
+            <button type="submit">Cadastrar</button>
+            </form>
+            </div>
+            <div class="linha">
+                <p class="p">Já possui uma conta? <a class="a" href="login.php">Faça login aqui!</a></p>
+            </div>
+        </div>
+        </div>
+        
+    </div>
+    </div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -9,28 +9,36 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="assets/css/loginEcadastro.css">
     
+    
     <link href="https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@100..900&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="container">
     '    <div class="left">
-            <h1 class="textao">Entre em sua conta!</h1>
+        <img src="assets/imagens/favicon.png" alt="Logo">
+        <div class="titulo">
+            <span>Bibliolandy</span>
         </div>
-        <div class="right">
+        <h1 class="textao">Entre em sua conta!</h1>
+    </div>
+    <div class="right">
+        <div class="chiclete">
             <form action="controllers/login.php" method="post">
-            
-                <div class="card">
+            <div class="card">
+            <form action="controllers/login.php" method="post">
+                
                     <label for="nome">Nome</label>
                     <input type="text" name="nome" required>
                     <label for="senha">Senha</label>
                     <input type="text" name="senha" required>
                     <button type="submit">Entrar</button>
-                </div>
-            
-            </form> 
-            <div class="linha">
-                <p class="p">Não possui uma conta? <a class="a" href="cadastro.php">Crie aqui!</a></p>
+                </form> 
             </div>
+            
+        <div class="linha">
+                <p class="p">Não possui uma conta? <a class="a" href="cadastro.php">Crie aqui!</a></p>
+            </div>  
+            </div>  
         </div>
     </div>
     
