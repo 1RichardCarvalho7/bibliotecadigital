5bd34e8e5cc5fa627b662ba3139763e16ff9c2cd
rafs00078000-eng <rafs00078000@gmail.com>
2026-06-04 21:14:29 -0300

    v5.3.6

--- controllers/login.php
+++ controllers/login.php
@@ -11,7 +11,7 @@ if (file_exists($arquivo)) {
 } else{die("Nenhum usuário cadastrado.");}
 $usuarios_json = json_decode($conteudo, true); // Converte o conteúdo em json para o padrão que o php entende (array associativo) O true faz o objeto ser convertido em array associativo. Quando está definido como falso retorna um objeto (padrão: false)
 
-if (!$usuarios_json === null){
+if ($usuarios_json === null){
     die("Arquivo de usuários inválido.");
 }
 
