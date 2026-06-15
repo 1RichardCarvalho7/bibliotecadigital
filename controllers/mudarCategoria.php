6929d255e156f43576cde270b8e2c8cc4bad2f7e
rafs00078000-eng <rafs00078000@gmail.com>
2026-06-15 09:28:14 -0300

    v5.6 (Botão para remover livro)

--- controllers/mudarCategoria.php
+++ controllers/mudarCategoria.php
@@ -1,16 +1,25 @@
 <?php 
 $id = $_POST['id'];
-$novaCategoria = $_POST['categoria'];
+$novaCategoria = $_POST['categoria']; // Pega o valor da categoria que o usuário quer mudar
 
 $livros = json_decode(file_get_contents('../data/books.json'), true);
 
-foreach ($livros as &$livro){
+foreach ($livros as $indice => $livro){
     if ($livro['id'] === $id){
-        $livro['categoria'] = $novaCategoria;
+        if ($novaCategoria == "remover"){
+            $arquivoImagem = '../uploads/' . $livro['capa']; //Remove a imagem da capa
+            if (file_exists($arquivoImagem)){
+                unlink($arquivoImagem);
+            }
+            unset($livros[$indice]);
+        } else{
+            $livro['categoria'] = $novaCategoria;
+        }
 
         break;
     }
 }
+
 
 file_put_contents('../data/books.json', json_encode($livros, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 
